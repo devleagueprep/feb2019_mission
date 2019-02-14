1b8b491d8aaf4f32ec113326b3c1ca65e1789b57
madycakes <viclee02@gmail.com>
2019-02-13 18:45:17 -1000

    adds exercises

---
+++ mission.js
@@ -0,0 +1,28 @@
+/*Congratulations Cool Kids, you've been selected for a top secret mission. Should you choose to accept it, do a git fetch and a git pull to find your specific mission. After you complete your mission, commit your work and do a pull request.
+
+This repo will self-destruct in five seconds. Good luck!*/
+
+
+/*Mission 1: 
+Team Leader: 
+
+@param Datatype: String
+@return Datatype: String
+
+Create a function named weatherReport that takes a parameter weather and returns the following: "Today's forecast is weather." Where your input will replace the weather parameter.
+Console.log to see your result.*/
+
+
+
+ /*Mission 2: 
+ Team Leader: 
+ 
+ @param Datatype: String
+ @param Datatype: Number
+ @return Datatype: String
+ 
+ 
+ Create a function named happyBday that takes two parameters name and age and returns the following: "Happy Birthday name! You look maaahvelous at age!" Where your input will replace the name and age parameters.
+ Console log to see your result.*/
+ 
+ 
